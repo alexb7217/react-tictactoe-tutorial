65155985640a371f9a0d5ffde350bb9589653842
alex <alexb7217@gmail.com>
2021-02-16 08:31:45 -0800

    Tics of tic tac toe

--- README.md
+++ README.md
@@ -1,17 +1,1 @@
-# react-for-dokku
-A simple deployment ready webpack-react application for dokku.
-
-## Instructions
-
-1.  Clone this repo
-2.  Run `npm install`
-3.  Run `npm run dev`, **localhost:8080** will open up in your default browser
-
-## Verify production code
-1. Run `webpack -p`
-2. Run `node server.js`, and visit **localhost:8080**, voila your code is ready for dokku now.
-
-## Prepare server for deployment
-1. On dokku server, prepare host environment
-2. On local set remote `git remote add dokku dokku@your-fqdn.net:yourappname`
-3. Run `git push dokku master`
+# Tic Tac Toe?
--- src/app.js
+++ src/app.js
@@ -1,9 +1,81 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
+import './app.css';
 
-const Index = () => {
-  return <div>React App With Webpack</div>;
+const Greeting = () => {
+  return <h1>Hello Tic Tac Toe!</h1>;
 };
 
-ReactDOM.render(<Index />, document.getElementById("index"));
+class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+  render() {
+    return (
+      <button className="square" 
+              onClick={() => this.setState({value: 'X'})}
+      >
+        {this.state.value}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square value={i} />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <Greeting />
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
